6e2f837409cb320be1676464cf294c2a927d6e62
Gonzalo Lucero <glucero6@hexacta.com>
2021-01-15 00:42:19 -0300

    Commit inicial

---
+++ ContactsApp/ContactsApp/ContactsApp/ViewModels/AddContactViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ContactsApp.ViewModels
+{
+    public class AddContactViewModel
+    {
+        string name = "Gonzalo Lucero";
+        string website = "https://github.com/LuceroGonzalo/CorvelDemo";
+        bool bestFriend;
+        bool isBusy;
+
+        public bool BestFriend
+        {
+            get { return bestFriend; }
+            set { bestFriend = value; }
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string DisplayMessage
+        {
+            get
+            {
+                return $"Your new friend is named {Name} and " +
+                       $"{(bestFriend ? "is" : "is not")} your best friend.";
+            }
+        }
+
+        public string Website
+        {
+            get { return website; }
+            set { website = value; }
+        }
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { isBusy = value; }
+        }
+
+        void LaunchWebsite()
+        {
+            try
+            {
+                Launcher.OpenAsync(new Uri(website));
+            }
+            catch
+            {
+
+            }
+        }
+
+        async Task SaveContact()
+        {
+            IsBusy = true;
+            await Task.Delay(4000);
+
+            IsBusy = false;
+
+            await Application.Current.MainPage.DisplayAlert("Save", "Contact has been saved", "OK");
+        }
+    }
+}
